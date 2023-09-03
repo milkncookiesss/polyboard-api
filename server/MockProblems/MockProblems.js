6eba06aaed2fdbb497b92c5f351edfc53cd17fae
milkncookiesss <p.parkcontact@gmail.com>
2023-09-03 08:51:30 -0700

    "added key for problems data"

--- server/MockProblems/MockProblems.js
+++ server/MockProblems/MockProblems.js
@@ -1,24 +1,26 @@
 let now = new Date();
 
-export default [
-  {
-    id: 1,
-    name: "get high",
-    createdByUser: "1",
-    routePath: ["Beta-A0-A", "Delta-A1-B", "Z-A4-A","W-A2-B"],
-    creatorNote: "I was going to clean my room",
-    weight: "0.5lb",
-    createdAt: now,
-    updatedAt: now
-  },
-  {
-    id: 2,
-    name: "get low",
-    createdByUser: "1",
-    routePath: ["A-A3-C", "D-A2-B", "G-A1-A", "H-A0-B"],
-    creatorNote: "3 6 9 dang she fine",
-    weight: "0.5lb",
-    createdAt: now,
-    updatedAt: now
-  }
-];
+export default {
+  problems: [
+    {
+      id: 1,
+      name: "get high",
+      createdByUser: "1",
+      routePath: ["Beta-A0-A", "Delta-A1-B", "Z-A4-A","W-A2-B"],
+      creatorNote: "I was going to clean my room",
+      weight: "0.5lb",
+      createdAt: now,
+      updatedAt: now
+    },
+    {
+      id: 2,
+      name: "get low",
+      createdByUser: "1",
+      routePath: ["A-A3-C", "D-A2-B", "G-A1-A", "H-A0-B"],
+      creatorNote: "3 6 9 dang she fine",
+      weight: "0.5lb",
+      createdAt: now,
+      updatedAt: now
+    }
+  ]
+};
