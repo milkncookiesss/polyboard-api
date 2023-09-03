76a317ee48ea1a562014bd448a2bd206a5ef3126
milkncookiesss <p.parkcontact@gmail.com>
2023-09-03 09:26:46 -0700

    "adding log"

--- server/ServiceLayer/problems/CreateProblems.js
+++ server/ServiceLayer/problems/CreateProblems.js
@@ -3,7 +3,7 @@ import * as DB from '../../DataLayer/Services/problems/CreateProblems.js';
 
 function createProblem() {
   return async (req, res, next) => {
-    // console.log('the body ', typeof req.body);
+    console.log(req.body);
     const { userId, routeName, routePath, creatorNote, weight, grade } = req.body;
     
     const problem = await DB.createProblem(JSON.parse(userId), JSON.parse(routePath), JSON.parse(weight), grade, JSON.parse(routeName), JSON.parse(creatorNote));
