1e38512993d71466775dfca054809bdfb5e2ef92
milkncookiesss <p.parkcontact@gmail.com>
2023-09-03 05:13:12 -0700

    "edit sql schema"

--- migrations/schema/schema.sql
+++ migrations/schema/schema.sql
@@ -23,40 +23,5 @@ CREATE TABLE "problems" (
   "update_at" timestamp
 );
 
--- CREATE TABLE "sends" (
---   "problem" integer,
---   "sentBy" integer,
---   "averageRating" integer,
---   "grade" varchar,
---   "note" varchar,
---   "created_at" timestamp
--- );
-
--- CREATE TABLE "followers" (
---   "user" integer,
---   "followingUser" integer,
---   "created_at" timestamp
--- );
-
--- CREATE TABLE "enums" (
---   "grades" varchar
--- );
-
--- CREATE TABLE "favorites" (
---   "userId" integer,
---   "problemId" integer
--- );
 
 ALTER TABLE "problems" ADD FOREIGN KEY ("createdByUser") REFERENCES "users" ("id");
-
--- ALTER TABLE "sends" ADD FOREIGN KEY ("problem") REFERENCES "problems" ("id");
-
--- ALTER TABLE "sends" ADD FOREIGN KEY ("sentBy") REFERENCES "users" ("id");
-
--- ALTER TABLE "followers" ADD FOREIGN KEY ("user") REFERENCES "users" ("id");
-
--- ALTER TABLE "followers" ADD FOREIGN KEY ("followingUser") REFERENCES "users" ("id");
-
--- ALTER TABLE "favorites" ADD FOREIGN KEY ("userId") REFERENCES "users" ("id");
-
--- ALTER TABLE "favorites" ADD FOREIGN KEY ("problemId") REFERENCES "problems" ("id");
