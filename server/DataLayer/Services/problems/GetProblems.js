db9bf7b00761faca2dd9db85950f16e2d5082e80
Peter Park <p.parkcontact@gmail.com>
2023-09-10 00:11:35 -0700

    "fetching all problems endpoint made"

--- server/DataLayer/Services/problems/GetProblems.js
+++ server/DataLayer/Services/problems/GetProblems.js
@@ -1,34 +1,22 @@
 import db from '../../models/index.js';
-// import problems from '../../../MockProblems/MockProblems.js';
 
 const Problem = db.Problem;
 const Sequelize = db.Sequelize;
 const Op = Sequelize.Op;
 
 /**
- * Get All Problems by userId
+ * Get All Problems
  */
 async function getAllProblems(offset, limit) {
   console.log('hello in data layer');
   
   try {
     const problems = await Problem.findAll({ order: [["createdAt", "DESC"]], raw: true });
+    console.log('the problems ', problems);
     return problems;
-    // return Problem.findAll({ order: [["createdAt", "DESC"]], raw: true });
   } catch(err) {
     throw err;
   }
-  // return problems;
-  // return Problem.findAll({
-  //   where: {
-  //     userId
-  //   },
-  //   offset,
-  //   limit,
-  //   attributes: ["id"],
-  //   order: [["createdAt", "DESC"]],
-  //   raw: true
-  // });
 }
 
 export { getAllProblems }
