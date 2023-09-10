635a54cb1bd1d5f5c80bec1baf2cdf748d0108f3
Peter Park <p.parkcontact@gmail.com>
2023-09-10 01:08:51 -0700

    "create problem api endpoint hitting db"

--- server/ServiceLayer/problems/CreateProblems.js
+++ server/ServiceLayer/problems/CreateProblems.js
@@ -1,14 +1,17 @@
 import * as DB from '../../DataLayer/Services/problems/CreateProblems.js';
 
-
+// -------------------------------------------------------------------------- //
+/**
+ * create problem service layer
+*/
 function createProblem() {
   return async (req, res, next) => {
     console.log(req.body);
-    const { userId, name="", routePath, creatorNote="", weight="", grade="" } = req.body;
-    console.log('-------> ', typeof routePath);
+    const { userId, name="", route, creatorNote="", weight="", grade="" } = req.body;
+    console.log('-------> ', typeof route);
     // let testPath = [];
     try{
-      const problem = await DB.createProblem(userId, routePath, weight, grade, name,creatorNote);
+      const problem = await DB.createProblem(userId, route, weight, grade, name,creatorNote);
       res.send({ problem: problem }).status(200);
       next();
     } catch (err) {
@@ -16,10 +19,6 @@ function createProblem() {
       res.status(500).send(err.message);
       next();
     }
-
-    // console.log('?????? ', problem);
-    // console.log(JSON.parse(problem));
-
   }
 }
 
