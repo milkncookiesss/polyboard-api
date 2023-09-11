02ed5304a3aaf44b6ab26c785484bc04dbaaf52b
Peter Park <p.parkcontact@gmail.com>
2023-09-11 16:51:57 -0700

    Implemented delete user and deleting problems user created
    "implemented delete user"
    
    "deleting users created problems when user is deleted"

---
+++ server/ServiceLayer/users/DeleteUser.js
@@ -0,0 +1,46 @@
+import * as DB from '../../DataLayer/Services/users/DeleteUser.js';
+import { GetUserInfo } from '../../DataLayer/Services/users/GetUserInfo.js';
+
+// -------------------------------------------------------------------------- //
+/**
+*/
+function deleteUser() {
+  return async (req, res, next) => {
+    const { userId } = req.body;
+    if (userId === "") {
+      res.status(400).send({ message: "no id was provided" });
+      return next();
+    }
+    const userExists = await checkUserExists(userId);
+    
+    if (!userExists) {
+      res.status(404).send({ message: "user was not found" });
+      return next();
+    }
+    try {
+      await DB.DeleteUser(userId);
+      await DB.DeleteUserProblems(userId);
+      res.status(200).send({});
+      next();
+    } catch(err) {
+      res.status(500).send({ message: "could not delete user" });
+      next();
+    }
+  }
+}
+
+// -------------------------------------------------------------------------- //
+/**
+*/
+async function checkUserExists(userId) {
+  const userInfo = await GetUserInfo(userId);
+  let userExists = false;
+  
+  if (userInfo) {
+    userExists = true;
+  }
+
+  return userExists;
+}
+
+export { deleteUser }
