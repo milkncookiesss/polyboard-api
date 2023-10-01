c2610e1e9b82deca4e5df43597e9d5ff3bfdb313
Peter Park <p.parkcontact@gmail.com>
2023-10-01 15:10:54 -0700

    implementing get sends by problem id

--- server/Api/problems.js
+++ server/Api/problems.js
@@ -3,6 +3,7 @@ import { getAllProblems } from "../ServiceLayer/problems/GetProblems.js";
 import { getAllUserSends } from "../ServiceLayer/sends/GetAllUserSends.js";
 import { getProblemById } from "../ServiceLayer/problems/GetProblemById.js";
 import { getProblemsByUser } from "../ServiceLayer/problems/GetProblemsByUser.js";
+
 import { createSend } from "../ServiceLayer/sends/CreateSend.js";
 import { createProblem } from "../ServiceLayer/problems/CreateProblems.js";
 import { deleteSend } from "../ServiceLayer/sends/DeleteSend.js";
@@ -14,6 +15,7 @@ const problemsRouter = Router();
 problemsRouter.get("/problems/getProblems", auth(), getAllProblems());
 problemsRouter.get("/problems/getProblemsFromUser", auth(), getProblemsByUser());
 problemsRouter.get("/problems/getAllUserSends", auth(), getAllUserSends());
+problemsRouter.get("/problems/getAllSendsByProblemId", auth(), );
 problemsRouter.get("/problems/getProblemById", auth(), getProblemById());
 problemsRouter.post("/problems/createProblem", auth(), createProblem());
 problemsRouter.post("/problems/createSend", auth(), createSend());
