ec2e566e5c2365696d550eadc843b38539930417
Peter Park <p.parkcontact@gmail.com>
2023-10-06 14:08:51 -0700

    implement request validation in problems

--- server/ServiceLayer/problems/DeleteProblemsFromUserId.js
+++ server/ServiceLayer/problems/DeleteProblemsFromUserId.js
@@ -1,3 +1,5 @@
+import Ajv from 'ajv';
+import addFormats from 'ajv-formats';
 import { DeleteProblemsFromUserId } from "../../DataLayer/Services/problems/DeleteProblemsFromUserId.js";
 import { GetProblemInfo, CheckUserOwnsProblem } from "../../DataLayer/Services/problems/GetProblemInfo.js";
 import { GetUserInfo } from "../../DataLayer/Services/users/GetUserInfo.js";
@@ -7,33 +9,34 @@ import { GetUserInfo } from "../../DataLayer/Services/users/GetUserInfo.js";
 */
 function deleteProblemsFromUserById() {
   return async (req, res, next) => {
-    const { userId } = req.body.user;
-    const { problemId } = req.body;
-
-    const userExists = await checkUserExists(userId);
-    const problemExists = await checkProblemExists(problemId);
-    const checkUserOwnsProblem = await checkProblemOwnedByUser(userId, problemId);
-    if (!userExists) {
-      res.status(404).send({ message: "user not found" });
-      return next();
-    }
-    if (!problemExists) {
-      res.status(404).send({ message: "problem not found" });
-      return next();
-    }
-    if (!checkUserOwnsProblem) {
-      res.status(401).send({ message: "cannot delete problem not created by user" });
-      return next();
-    }
-
     try {
+      await validateRequest(req.body);
+      const { userId } = req.body.user;
+      const { problemId } = req.body;
+
+      const userExists = await checkUserExists(userId);
+      const problemExists = await checkProblemExists(problemId);
+      const checkUserOwnsProblem = await checkProblemOwnedByUser(userId, problemId);
+      if (!userExists) {
+        res.status(404).send({ statusCode: 404, message: "user not found" });
+        return next();
+      }
+      if (!problemExists) {
+        res.status(404).send({ statusCode: 404, message: "problem not found" });
+        return next();
+      }
+      if (!checkUserOwnsProblem) {
+        res.status(401).send({ statusCode: 401, message: "cannot delete problem not created by user" });
+        return next();
+      }
+
       await DeleteProblemsFromUserId(userId, problemId);
       res.status(200).send(true);
       next();
     } catch(err) {
-      res.status(500).send(err);
+      console.error("Error in DeleteProblemsFromUserId ", err);
+      res.status(500).send({ statusCode: 500, message: "Could not delete problem."});
       next()
-      throw err;
     }
   }
 }
@@ -75,6 +78,46 @@ async function checkProblemOwnedByUser(userId, problemId) {
   }
 
   return userOwnsProblem;
+}
+
+// -------------------------------------------------------------------------- //
+/**
+*/
+async function validateRequest(request) {
+  const ajv = new Ajv();
+
+  const schema = {
+    type: "object",
+    properties: {
+      problemId: {
+        type: "string",
+        format: "uuid"
+      },
+      user: {
+        type: "object",
+        properties: {
+          userId: {
+            type: "string",
+            format: "uuid"
+          },
+          role: {
+            type: "string"
+          }
+        }
+      }
+    },
+    required: ["problemId", "user"],
+    additionalProperties: false
+  };
+
+  addFormats(ajv);
+  const validate = ajv.compile(schema);
+  const valid = validate(request);
+
+  if (!valid) {
+    console.error('AJV error ', validate.errors[0].message);
+    throw validate.errors[0];
+  }
 }
 
 export { deleteProblemsFromUserById };
